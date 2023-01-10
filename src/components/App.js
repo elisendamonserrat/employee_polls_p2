c6305c083545cecf5bd387e73125996d3e16be15
elisenda <elisenda.monserrat@gmail.com>
2023-01-10 20:25:45 +0100

    feat: adds all five pages of the app with basic HTML struture and defines routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import Login from "../pages/Login";
 import "../styles/index.css";
 import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import QuestionPage from "../pages/QuestionPage";
+import NewPollPage from "../pages/NewPollPage";
+import LeaderBoardPage from "../pages/LeaderBoardPage";
 
 function App() {
   return (
     <>
       <div className="container">
         <Routes>
+          <Route path="/" exact element={<HomePage />} />
           <Route path="/login" exact element={<Login />} />
+          <Route path="/questions/:question_id" element={<QuestionPage />} />
+          <Route path="/add" exact element={<NewPollPage />} />
+          <Route path="/leaderboard" exact element={<LeaderBoardPage />} />
         </Routes>
       </div>
     </>
---
+++ src/pages/NewPollPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewPollPage = () => {
+  return (
+    <div>New Poll Page</div>
+  )
+}
+
+export default NewPollPage

0f9b1aead90a18f5e83f26f5cf9bd088b125dbe9
elisenda <elisenda.monserrat@gmail.com>
2023-01-15 18:50:21 +0100

    feat: finishes PollResults logic and styles

--- src/components/PollResults.js
+++ src/components/PollResults.js
@@ -7,42 +7,51 @@ const PollResults = ({ question, selectedOption }) => {
 
   const totalVotes = sum(optionOne.votes.length, optionTwo.votes.length);
 
+  const selectedOptionsStyles =
+    "bg-blue-500 text-white border-b-4 border-blue-500 flex items-center justify-between";
   return (
     <>
       <p>Poll already answered. See below the stats!</p>
-      <div className="flex w-full justify-between">
-        <div className="w-48 bg-transparent text-blue-700 font-semibold border border-blue-500 rounded text-center">
-          <div className="py-2 px-2">{capitalizeStr(optionOne.text)}</div>
-          <div className="bg-blue-100 py-2 px-4">
-            <span className="flex items-center justify-center">
-              {selectedOption === "optionOne" ? (
-                <HiCheck className="h-6 w-8 text-green-500" />
-              ) : (
-                ""
-              )}
-              Selected
+      <div className="flex flex-col w-11/12">
+        <div className="mt-2 flex flex-col items-center space-y-4 w-full">
+          <div
+            className={`bg-blue-50 py-4 px-2 w-full md:w-7/12 rounded hover:cursor-pointer border-b-4  ${
+              selectedOption === "optionOne" && selectedOptionsStyles
+            }`}
+          >
+            <span className="ml-2">{capitalizeStr(optionOne.text)}</span>
+            {selectedOption === "optionOne" && (
+              <HiCheck className="h-6 w-8 text-green-500" />
+            )}
+          </div>
+          <div className="bg-blue-500 py-4 px-2 w-full md:w-7/12 rounded hover:cursor-pointer">
+            <span className="ml-2 text-white font-semibold">
+              {optionOne.votes.length}/{totalVotes} Answers
+            </span>
+            <span className="mx-2 text-white font-semibold">|</span>
+            <span className="text-white font-semibold">
+              {calculatePercentage(optionOne.votes.length, totalVotes)}% Votes
             </span>
           </div>
-          <div className="bg-blue-400 py-2 px-4 text-white">
-            Total Votes {optionOne.votes.length} (
-            {calculatePercentage(optionOne.votes.length, totalVotes)}%)
+
+          <div
+            className={`bg-blue-50 py-4 px-2 w-full md:w-7/12 rounded hover:cursor-pointer border-b-4  ${
+              selectedOption === "optionTwo" && selectedOptionsStyles
+            }`}
+          >
+            <span className="ml-2">{capitalizeStr(optionTwo.text)}</span>
+            {selectedOption === "optionTwo" && (
+              <HiCheck className="h-6 w-8 text-green-500" />
+            )}
           </div>
-        </div>
-        <div className="w-48 bg-transparent text-blue-700 font-semibold border border-blue-500 rounded text-center">
-          <div className=" py-2 px-4 ">{capitalizeStr(optionTwo.text)}</div>
-          <div className="bg-blue-100 py-2 px-4">
-            <span className="flex items-center justify-center space-x-1">
-              {selectedOption === "optionTwo" ? (
-                <HiCheck className="h-6 w-8 text-green-500" />
-              ) : (
-                ""
-              )}
-              Selected
+          <div className="bg-blue-500 py-4 px-2 w-full md:w-7/12 rounded hover:cursor-pointer">
+            <span className="ml-2 text-white font-semibold">
+              {optionTwo.votes.length}/{totalVotes} Answers
             </span>
-          </div>
-          <div className="bg-blue-400 py-2 px-4 text-white">
-            Total Votes {optionTwo.votes.length} (
-            {calculatePercentage(optionTwo.votes.length, totalVotes)}%)
+            <span className="mx-2 text-white font-semibold">|</span>
+            <span className="text-white font-semibold">
+              {calculatePercentage(optionTwo.votes.length, totalVotes)}% Votes
+            </span>
           </div>
         </div>
       </div>
